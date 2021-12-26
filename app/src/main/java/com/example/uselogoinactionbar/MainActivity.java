af6f2a73e8633edc22524b733757fa7e5b80855d
sajibsaha4647 <sajibsahabdtask@gmail.com>
2021-12-26 13:25:24 +0600

    adding logo in actions bar

---
+++ app/src/main/java/com/example/uselogoinactionbar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.uselogoinactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
+    }
+}
